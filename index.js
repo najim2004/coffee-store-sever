75b2691f4dc5a087c3d897c3e6c1f6c00631d6c2
najim2004 <najim10112004@gmail.com>
2024-10-04 17:02:45 +0600

    test 1

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -30,20 +38,38 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
-    const database = client.db("coffeeStoreDB").collection("coffees");
+    const coffeeCollection = client.db("coffeeStoreDB").collection("coffees");
+    const usersCollection = client.db("coffeeStoreDB").collection("users");
+
+    // auth related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log("42", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+          // sameSite: "none",
+        })
+        .send({ success: true });
+    });
 
     // get all coffees
     app.get("/coffees", async (req, res) => {
-      const coffees = await database.find().toArray();
+      const coffees = await coffeeCollection.find().toArray();
+      console.log("cookies", req.cookies.token);
       res.json(coffees);
     });
 
     // get single coffee
     app.get("/coffees/:id", async (req, res) => {
       const id = req.params.id;
-      const coffee = await database.findOne({ _id: new ObjectId(id) });
+      const coffee = await coffeeCollection.findOne({ _id: new ObjectId(id) });
       res.json(coffee);
     });
 
@@ -51,7 +77,7 @@ async function run() {
     app.post("/coffees", async (req, res) => {
       const newCoffee = req.body;
       console.log("this is new coffee:", newCoffee);
-      const result = await database.insertOne(newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee);
       res.json(result);
     });
 
@@ -73,7 +99,11 @@ async function run() {
           Photo: coffee.Photo,
         },
       };
-      const result = await database.updateOne(findId, updatedCoffee, options);
+      const result = await coffeeCollection.updateOne(
+        findId,
+        updatedCoffee,
+        options
+      );
       res.json(result);
     });
 
@@ -81,7 +111,19 @@ async function run() {
     app.delete("/coffees/:id", async (req, res) => {
       const id = req.params.id;
       const findId = { _id: new ObjectId(id) };
-      const result = await database.deleteOne(findId);
+      const result = await coffeeCollection.deleteOne(findId);
+      res.json(result);
+    });
+
+    // user related apis
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const result = await usersCollection.insertOne(newUser);
+      res.json(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
       res.json(result);
     });
 
